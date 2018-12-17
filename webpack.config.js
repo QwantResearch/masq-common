180870c6bc5ffcf64fe5cf6df06f523aab6ebd5d
Levent DEMIR <l.demir@qwant.com>
2018-12-17 15:41:54 +0100

    remove log

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
-var path = require('path')
-
 module.exports = {
   entry: {
     MasqCommon: './dist/index.js'
   },
   output: {
     library: 'MasqCommon'
-    // filename: './[name].bundle.js'
   }
 }
