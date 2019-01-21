58e193bc9a38886550470e893398440c858a2615
Levent DEMIR <l.demir@qwant.com>
2019-01-21 18:01:48 +0100

    use expect.to.throw, refactor

--- test/errors.js
+++ test/errors.js
@@ -1,64 +1,43 @@
+// import { MasqError } from '../src/errors'
+
 /* eslint-env mocha */
 /* global MasqCommon */
 /* global chai */
+const { WRONG_PARAMETER } = MasqCommon.errors.ERRORS
+const { checkObject } = MasqCommon.errors
 
 describe('MasqCommon errors', () => {
   context('CheckObject', () => {
     it('Should not throw ', async () => {
       const myObj = { name: 'Bob', age: 18 }
       const requiredParameters = ['age', 'name']
-      let err = { type: '_ERROR_NOT_THROWN_' }
-      try {
-        MasqCommon.errors.checkObject(myObj, requiredParameters)
-      } catch (error) {
-        err.type = '_ERROR_THROWN'
-      }
-      chai.assert.equal(err.type, '_ERROR_NOT_THROWN_', 'Should not reject')
+      const toCall = () => checkObject(myObj, requiredParameters)
+      chai.expect(toCall).to.not.throw()
     })
     it('Should not throw if a property has an empty string', async () => {
       const myObj = { name: '', age: 18 }
       const requiredParameters = ['age', 'name']
-      let err = { type: '_ERROR_NOT_THROWN_' }
-      try {
-        MasqCommon.errors.checkObject(myObj, requiredParameters)
-      } catch (error) {
-        err.type = '_ERROR_THROWN'
-      }
-      chai.assert.equal(err.type, '_ERROR_NOT_THROWN_', 'Should not reject')
+      const toCall = () => checkObject(myObj, requiredParameters)
+      chai.expect(toCall).to.not.throw()
     })
     it('Should not throw if a property has an object', async () => {
       const myObj = { name: { id: 'id' }, age: 18 }
       const requiredParameters = ['age', 'name']
-      let err = { type: '_ERROR_NOT_THROWN_' }
-      try {
-        MasqCommon.errors.checkObject(myObj, requiredParameters)
-      } catch (error) {
-        err.type = '_ERROR_THROWN'
-      }
-      chai.assert.equal(err.type, '_ERROR_NOT_THROWN_', 'Should not reject')
+      const toCall = () => checkObject(myObj, requiredParameters)
+      chai.expect(toCall).to.not.throw()
     })
     it('Should throw if any property is not set ', async () => {
       const myObj = { age: 18 }
       const requiredParameters = ['age', 'name']
-      let err = { type: '_ERROR_NOT_THROWN_' }
-      try {
-        MasqCommon.errors.checkObject(myObj, requiredParameters)
-      } catch (error) {
-        err = error
-      }
-      chai.assert.equal(err.type, MasqCommon.errors.ERRORS.WRONG_PARAMETER, 'Should not reject')
+      const toCall = () => checkObject(myObj, requiredParameters)
+      chai.expect(toCall).to.throw().with.property('type', WRONG_PARAMETER)
     })
 
     it('Should throw if any property is undefined ', async () => {
       const myObj = { name: undefined, age: 18 }
       const requiredParameters = ['age', 'name']
-      let err = { type: '_ERROR_NOT_THROWN_' }
-      try {
-        MasqCommon.errors.checkObject(myObj, requiredParameters)
-      } catch (error) {
-        err = error
-      }
-      chai.assert.equal(err.type, MasqCommon.errors.ERRORS.WRONG_PARAMETER, 'Should not reject')
+      const toCall = () => checkObject(myObj, requiredParameters)
+      chai.expect(toCall).to.throw().with.property('type', WRONG_PARAMETER)
     })
   })
 })
