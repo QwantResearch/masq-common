fd930d250048530e7fa97d34062f51d9417df733
Levent DEMIR <l.demir@qwant.com>
2019-01-21 17:08:05 +0100

    change checkObject behaviour, throw only if undefined

--- dist/errors.js
+++ dist/errors.js
@@ -88,7 +88,7 @@ var MasqError = function (_Error) {
 var checkObject = function checkObject(obj, parameters) {
   for (var i in parameters) {
     if (typeof obj[parameters[i]] !== 'boolean') {
-      if (!obj[parameters[i]] || obj[parameters[i]] === '') {
+      if (obj[parameters[i]] === undefined) {
         throw new MasqError(ERRORS.WRONG_PARAMETER, 'The parameter ' + parameters[i] + ' is required');
       }
     }
