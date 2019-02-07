ad0dc8061cf7b643bcfe699749f9a5b80bd8d943
G-Ray <g.bonneville@qwant.com>
2019-02-07 17:28:23 +0100

    Remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,11 @@ var path = require('path')
 
 module.exports = {
   entry: './src/index.js',
-  target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'masq-common.js',
     library: 'MasqCommon',
     libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      }
-    ]
   },
   node: {
     fs: 'empty'
