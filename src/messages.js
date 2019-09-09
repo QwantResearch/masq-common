0dfbc856722f1b7ac0993f433272fadc97036e7a
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-09-09 12:41:46 +0200

    index.js, messages.js: Define message types to be used in exchanges between user-app and masq-app

---
+++ src/messages.js
@@ -0,0 +1,14 @@
+const UserAppLogin = {
+  AUTHORIZED: 'authorized',
+  NOT_AUTHORIZED: 'notAuthorized',
+  REGISTER_USER_APP: 'registerUserApp',
+  MASQ_ACCESS_GRANTED: 'masqAccessGranted',
+  MASQ_ACCESS_REFUSED: 'masqAccessRefused',
+  REQUEST_WRITE_ACCESS: 'requestWriteAccess',
+  WRITE_ACCESS_GRANTED: 'writeAccessGranted',
+  CONNECTION_ESTABLISHED: 'connectionEstablished'
+}
+
+export {
+  UserAppLogin
+}
